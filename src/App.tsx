d55b161b0476dfffa1321d83dfc59382bad56f8b
its-rayan <rayanmabdirahman@gmail.com>
2026-02-06 21:22:24 +0000

    initial commit with shadcn and tailwind config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="dark bg-background text-foreground">
+      <div className="max-w-7xl mx-auto flex min-h-screen flex-col px-6 py-12">
+        <h1 className="text-2xl font-medium">React AG Grid Example</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
