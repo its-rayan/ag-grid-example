e8c5e37d57a791fd1f847d024c09fd7eddcd1bb9
its-rayan <rayanmabdirahman@gmail.com>
2026-02-07 14:12:02 +0000

    add boilerplate code for grid example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import GridExample from "./components/GridExample";
+
 function App() {
   return (
     <main className="dark bg-background text-foreground">
-      <div className="max-w-7xl mx-auto flex min-h-screen flex-col px-6 py-12">
+      <div className="max-w-7xl mx-auto flex min-h-screen flex-col px-6 py-12 gap-10">
         <h1 className="text-2xl font-medium">React AG Grid Example</h1>
+        <GridExample />
       </div>
     </main>
   );
---
+++ src/components/GridExample/index.tsx
@@ -0,0 +1,36 @@
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+import { AgGridReact } from "ag-grid-react";
+
+const GridExample = () => {
+  const defaultColDef = {
+    flex: 1,
+  };
+
+  // Container: Defines the grid's theme & dimensions.
+  return (
+    <div className="h-screen w-full">
+      <AgGridReact
+        rowData={[
+          { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+          { make: "Ford", model: "F-Series", price: 33850, electric: false },
+          { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+          { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+          { make: "Fiat", model: "500", price: 15774, electric: false },
+          { make: "Nissan", model: "Juke", price: 20675, electric: false },
+        ]}
+        columnDefs={[
+          { field: "make" },
+          { field: "model" },
+          { field: "price" },
+          { field: "electric" },
+        ]}
+        defaultColDef={defaultColDef}
+      />
+    </div>
+  );
+};
+
+export default GridExample;
