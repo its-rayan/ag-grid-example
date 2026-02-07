aae7121503867c424bc6345265d144da9ac2821a
its-rayan <rayanmabdirahman@gmail.com>
2026-02-07 18:53:12 +0000

    add custom tailwindcss to grid

--- src/components/GridExample/index.tsx
+++ src/components/GridExample/index.tsx
@@ -1,5 +1,9 @@
 import generateTrades from "@/data/generateTrades";
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  type ColDef,
+} from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 import { useMemo } from "react";
 
@@ -8,35 +12,37 @@ ModuleRegistry.registerModules([AllCommunityModule]);
 
 const GridExample = () => {
   // generate 10000 trades for the grid to demonstrate performance with large datasets
-  const rowData = useMemo(() => generateTrades(), []);
-  console.log({ rowData });
+  const rowData = useMemo(() => generateTrades(10), []);
+
+  // Define the columns to be rendered in the grid, along with any custom formatting for the values.
+  const colomnDefs: ColDef[] = [
+    { field: "id", headerName: "Id", hide: true },
+    {
+      field: "time",
+      headerName: "Time",
+      valueFormatter: (params) =>
+        new Date(params.value).toLocaleString().replace(", ", " - "),
+    },
+    { field: "stock_symbol", headerName: "Stock Symbol" },
+    { field: "stock_name", headerName: "Stock Name" },
+    { field: "quantity", headerName: "Quantity" },
+    {
+      field: "price",
+      headerName: "Price",
+      valueFormatter: (params) => `$${params.value}`,
+    },
+    { field: "status", headerName: "Status" },
+  ];
 
   const defaultColDef = {
     flex: 1,
   };
 
-  // Container: Defines the grid's theme & dimensions.
   return (
     <div className="h-screen w-full">
       <AgGridReact
         rowData={rowData}
-        columnDefs={[
-          { field: "id", headerName: "ID" },
-          {
-            field: "time",
-            headerName: "Time",
-            valueFormatter: (params) => new Date(params.value).toLocaleString(),
-          },
-          { field: "stock_symbol", headerName: "Stock Symbol" },
-          { field: "stock_name", headerName: "Stock Name" },
-          { field: "quantity", headerName: "Quantity" },
-          {
-            field: "price",
-            headerName: "Price",
-            valueFormatter: (params) => `$${params.value}`,
-          },
-          { field: "status", headerName: "Status" },
-        ]}
+        columnDefs={colomnDefs}
         defaultColDef={defaultColDef}
       />
     </div>
